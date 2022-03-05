d58703d4a11c2ada0f4928e01d4791da3353d7dc
mougang <sunnyxxiong@outlook.com>
2022-03-05 13:55:26 +0800

    feat: video

--- src/pages/levelTwo/levelTwoS1.vue
+++ src/pages/levelTwo/levelTwoS1.vue
@@ -1,35 +1,47 @@
 <template>
-<div class="twoWrap1">
-  <video width="100%" height="100%" controls="controls" :onended="stopVideo()" v-show="dshow1">
-  <source src="../../assets/homeTv.mp4" type="video/mp4" />
-  </video>
-  <img src="../../assets/daddy.png" width="80%" height="100%" v-show="dshow2" style="margin-left:10%">
-</div>
+  <div class="twoWrap1">
+    <video
+      width="100%"
+      height="100%"
+      controls="controls"
+      @ended="stopVideo()"
+      v-show="dshow1"
+    >
+      <source src="../../assets/homeTv.mp4" type="video/mp4" />
+    </video>
+    <img
+      src="../../assets/daddy.png"
+      width="80%"
+      height="100%"
+      v-show="dshow2"
+      style="margin-left: 10%"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'levelTwoS1',
 
-data(){
-  return{
-    dshow1:true,
-    dshow2:false
+  data () {
+    return {
+      dshow1: true,
+      dshow2: false
 
-  }
-},
+    }
+  },
 
-methods:{
-  stopVideo(){
-    this.dshow1=false
-    this.dshow2=true
+  methods: {
+    stopVideo () {
+      this.dshow1 = false
+      this.dshow2 = true
+    }
   }
-}
 }
 </script>
 
 <style>
-.twoWrap1{
+.twoWrap1 {
   width: 100%;
   height: 100%;
   background: black;
